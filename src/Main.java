6c0521f0de9a90501fb560afd89956a9c62d0036
jibachh <jibachhmahato@gmail.com>
2023-09-26 22:44:54 +0545

    AutomaticExpression

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[]args){
-        System.out.println("hello jibachh");
+        int a = 256;
+        byte b = (byte)(a);
+        System.out.println(b);
 
     }
 }
