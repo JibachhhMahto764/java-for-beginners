55933ee81c7e4b4d39dcebb9a2a7b5040a9e9c5d
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2023-10-06 08:13:36 +0545

    Create Non-Primitive-Datatype.java

---
+++ Non-Primitive-Datatype.java
@@ -0,0 +1,9 @@
+// Breaks parts by partys (individual character)
+// such as: String value
+
+
+public class NonPrimitivesn {
+public static void main(String[] args ) {
+String name = " Jibachh Singh";
+}
+}
