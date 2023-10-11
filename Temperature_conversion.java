a6641494ef3f81c5cefcf00cc926b63cc9154ec4
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2023-10-11 08:25:28 +0545

    Create Temperature_conversion.java

---
+++ Temperature_conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Temperature{
+  public static void Main(string[] args){
+    Scanner in = new Scanner(system.in);
+    system.out.println(Please enter temp in C: ");
+    float tempC = in.nextFloat();
+    float tempF = (tempC*9/5)+32);
+    system.out.println(tempF);
+  }
+}
+}
