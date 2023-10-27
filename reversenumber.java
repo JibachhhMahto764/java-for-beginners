0dda52428335726ef49d3085beb75d990b2c887e
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2023-10-27 23:32:37 +0545

    Create reversenumber.java

---
+++ reversenumber.java
@@ -0,0 +1,12 @@
+public class Reverse{
+  public static void main (string[] args){
+    int num = 23597;
+    int ans = 0;
+    while(n>0){
+      int rem = num%10 ;
+      num /= 10;
+      ans = ans*10 + rem;
+    }
+    system.out.println(ans);
+  }
+}
