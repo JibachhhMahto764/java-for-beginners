2f2b425d033252679a83b182117fec7b875b2356
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2025-02-07 21:34:55 +0545

    add garbage collection

--- OOPs/WrapperExample.java
+++ OOPs/WrapperExample.java
@@ -12,11 +12,17 @@ public class WrapperExample {
         System.out.println(a + " " + b);
 //        final int bonus = 2;
 //        bonus = 3;// you can't modifie the final keyword data
-       
+
         final  A jibachh = new A("jibachh singh");
       jibachh.name = "other name";
       //jibachh = new A("new object");// i can't do this
         // when a non primitive is final you cannot reassign it
+
+        A obj;
+        // use fro garbage collection
+        for (int i= 0; i < 1000000000;i++){
+            obj = new A("random name");
+        }
     }
      static  void swap(Integer a,Integer b){
         Integer temp = a;
@@ -29,6 +35,13 @@ class  A{
     final int num = 10; // :: always intialize it while declarait it
       String name;
   public A(String name){
+      System.out.println("object created");
       this.name = name;
   }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed");
+    }
 }
